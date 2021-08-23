580e7a2dd34deb63e65820fc72043940f4c9740f
boss1000 <etchuakem81@gmail.com>
2021-08-23 15:57:31 -0700

    updating app

--- server/submit_campaign.php
+++ server/submit_campaign.php
@@ -47,9 +47,8 @@
 				//media is an image 
 				$image=$_FILES['file'];
 				$image_name=$_FILES['file']['name'];
-				$image_folder="../img/campaign_img/";//campaign_images
-				echo is_writable('$image_folder');
-				//$media_file=uploader($image_name,$image,$image_folder);
+				$image_folder="campaign_img/";//campaign_images
+				$media_file=uploader($image_name,$image,$image_folder);
 				//imageRotator($media_file,$image_name,$image_folder);
 				//db_store($media_file);
 			}
