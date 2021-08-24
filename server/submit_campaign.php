0ef427d679d666c392d1ccc2155db98e158142e1
boss1000 <etchuakem81@gmail.com>
2021-08-24 00:05:55 -0700

    updating app

--- server/submit_campaign.php
+++ server/submit_campaign.php
@@ -47,11 +47,10 @@
 				//media is an image 
 				$image=$_FILES['file'];
 				$image_name=$_FILES['file']['name'];
-				$image_folder="campaigns/img/";//campaign_images
+				$image_folder="../campaigns/img/";//campaign_images
 				$media_file=uploader($image_name,$image,$image_folder);
 				//imageRotator($media_file,$image_name,$image_folder);
 				//db_store($media_file);
-			}
 			
 		}
 		
