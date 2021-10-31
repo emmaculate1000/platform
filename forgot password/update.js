95a4131113a7b7a6dd7f8926c722fade72e6f099
boss1000 <etchuakem81@gmail.com>
2021-10-31 23:24:25 +0100

    ud

---
+++ forgot password/update.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	//user submits login form
+	$(".login100-form").submit(function(e){
+		e.preventDefault();
+		//get user info.
+		let email=$("#email").val();
+		let password=$("#password").val();
+		if(email!==""&&password!==""){
+			$.post(
+				'login.php',
+				{
+					"email": email,
+					"password":password,
+				},
+				function(data,status){
+					console.log(data);
+					if(status=='success'){
+						if(data==1){
+							//redirect to dashboard
+							window.location="http://digitad.us-east-2.elasticbeanstalk.com";
+						}else{
+							//clear input fields
+							$(".response").show(100).html("Invalid Email / Password")
+							.fadeOut(10000);
+						}
+					}
+				}
+			)
+		}
+	})
+})
