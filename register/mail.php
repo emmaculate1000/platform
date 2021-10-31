ae7d51bfe4dd55e87c6628ca9711739af62dc69a
boss1000 <etchuakem81@gmail.com>
2021-10-31 22:14:50 +0100

    updating app

--- register/mail.php
+++ register/mail.php
@@ -9,7 +9,7 @@
 	    require 'PHPMailer/SMTP.php';
         // Replace sender@example.com with your "From" address.
         // This address must be verified with Amazon SES.
-        $sender = "no-reply@digitad-ooh.com";
+        $sender = "akem@digitad-ooh.com";
         $senderName = "Digit-AD";
 
         // Replace recipient@example.com with a "To" address. If your account
