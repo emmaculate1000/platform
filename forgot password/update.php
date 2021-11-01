2abca249bbb19c3b4379f2cb2edb140de6ca7f62
boss1000 <etchuakem81@gmail.com>
2021-11-01 02:50:34 +0100

    updating app

--- forgot password/update.php
+++ forgot password/update.php
@@ -5,36 +5,40 @@
 	//include config file
 	$configs=include('../config.php');
 	if(isset($_SESSION['reset_credentials'])){
-		$userCredentials=$_SESSION['reset_credentials'];
-        $credentials=explode('-',$userCredentials);
-        $passwordX=$credentials[0];
-		$email=$credentials[1];
-		//connect to database
-		try{
-			//connect to datatbase
-		    $db_host=$configs['db_configs']['host'];
-	        $db_user=$configs['db_configs']['user'];
-	        $db_password=$configs['db_configs']['password'];
-	        $db=$configs['db_configs']['users_db'];
-	       $connection=mysqli_connect($db_host,$db_user,$db_password,$db);
-            if(mysqli_connect_error()){
-                throw new Exception(mysqli_connect_error());
-            }
-            if($connection){
-            	$emailX=mysqli_real_escape_string($connection,$email);
-            	$sql="UPDATE digitad SET password='$passwordX' WHERE email='$emailX'";
-            	$query=mysqli_query($connection,$sql);
-            	if(!$query){
-            		throw new Exception(mysqli_error($query));
-            	}else{
-                   echo 1;
-                }
-            }else{
-				echo "no connection";
-			}
+		if(isset($_POST['password'])){
+			$password=cleaner($_POST['password']);
+			$userCredentials=$_SESSION['reset_credentials'];
+			$credentials=explode('-',$userCredentials);
+			$passwordX=$credentials[0];
+			$email=$credentials[1];
+			//connect to database
+			try{
+				//connect to datatbase
+				$db_host=$configs['db_configs']['host'];
+				$db_user=$configs['db_configs']['user'];
+				$db_password=$configs['db_configs']['password'];
+				$db=$configs['db_configs']['users_db'];
+			$connection=mysqli_connect($db_host,$db_user,$db_password,$db);
+				if(mysqli_connect_error()){
+					throw new Exception(mysqli_connect_error());
+				}
+				if($connection){
+					$emailX=mysqli_real_escape_string($connection,$email);
+					$passwordY=md5(mysqli_real_escape_string($connection,$password));
+					$sql="UPDATE digitad SET password='$passwordY' WHERE email='$emailX'";
+					$query=mysqli_query($connection,$sql);
+					if(!$query){
+						throw new Exception(mysqli_error($query));
+					}else{
+					echo 1;
+					}
+				}else{
+					echo "no connection";
+				}
 
-		}catch(Exception $e){
-			print_r($e);
+			}catch(Exception $e){
+				print_r($e);
+			}
 		}
 	}
 ?>
