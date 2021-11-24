c548d2c676476d6998fea5b5c2adfcd61f05713f
emmaculate1000 <emmaculateakem@gmail.com>
2021-11-24 18:25:06 +0100

    update

--- server/fundWallet.php
+++ server/fundWallet.php
@@ -6,7 +6,26 @@
             $amount=$_POST['amount'];
             $user_id=$_POST['user_id'];
             $email=$_POST['email'];
-            echo $amount;
+            $created=date("d-m-Y");
+            //database configs
+            $configs=include('config.php');
+            $db_host=$configs['db_configs']['host'];
+            $db_user=$configs['db_configs']['user'];
+            $db_password=$configs['db_configs']['password'];
+            $db=$configs['db_configs']['users_db'];
+            $smtpConfig=$configs['smtp_cred'];
+            //connect to database
+            try{
+                $connection=mysqli_connect($db_host,$db_user,$db_password,$db);
+                if(mysqli_connect_error()){
+                    throw new Exception(mysqli_connect_error());
+                }
+                if($connection){
+                    echo "connected!";
+                }
+            }catch(Exception $e){
+                print_r($e);
+            }
         }
     }
 ?>
