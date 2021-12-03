0eeec859179e24c107e00e74630c7d457b07bbe7
emmaculate1000 <emmaculateakem@gmail.com>
2021-12-03 07:03:27 +0100

    update

--- server/coupon.php
+++ server/coupon.php
@@ -4,13 +4,9 @@
     ];
     if($_SERVER['REQUEST_METHOD']=='POST'){
         if(isset($_POST['coupon'])){
-            echo 'yes';
             $coupon=$_POST['coupon'];
-            echo $coupon;
-            if(in_array($coupon,$coupons)){
+            if(array_key_exists($coupon,$coupons)){
                 echo 'sure';
-            }else{
-                echo 'no';
             }
         }
     }
