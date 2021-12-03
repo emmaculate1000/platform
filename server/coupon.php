85f4b90f8add3a1d63272f810bc08f3ea2d61781
emmaculate1000 <emmaculateakem@gmail.com>
2021-12-03 06:41:35 +0100

    update

--- server/coupon.php
+++ server/coupon.php
@@ -2,8 +2,11 @@
     $coupons=[
         "kxy"=>10
     ];
+    echo 'ok';
     if($_SERVER['REQUEST_METHOD']=='GET'){
+        echo 'yeah';
         if(isset($_GET['coupon'])){
+            echo 'yes';
             $coupon=$_GET['coupon'];
             echo $coupon;
             /*if(in_array($coupon,$coupons)){
