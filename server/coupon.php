4faf0a4b28792c36c38162e743640c087d404e7f
emmaculate1000 <emmaculateakem@gmail.com>
2021-12-03 06:17:45 +0100

    update

--- server/coupon.php
+++ server/coupon.php
@@ -5,12 +5,11 @@
     if($_SERVER['REQUEST_METHOD']=='GET'){
         if(isset($_GET['coupon'])){
             $coupon=$_GET['coupon'];
-            echo $coupon;
-            /*if(in_array($coupon,$coupons)){
+            if(in_array($coupon,$coupons)){
                 echo 'yes';
             }else{
                 echo 'no';
-            }*/
+            }
         }
     }
 ?>
